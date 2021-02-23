ba30b3e2d9e82ca0c6f429b9c6c2bb9763e8c3cb
lyhhh <17610603221@163.com>
2021-02-23 15:44:45 +0800

    update

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
-import * as commander from 'commander';
-import {translate} from './main';
+import * as commander from "commander";
+import { translate } from "./main";
 
 const program = new commander.Command();
 
-program.version('0.0.6')
-	.name('tt')
-	.usage('<english>')
-	.arguments('<english>')
+program
+	.version("0.0.6")
+	.name("tt")
+	.usage("<english>")
+	.arguments("<english>")
 	.action(function (english) {
-		translate(english)
+		translate(english);
 	});
 
 program.parse(process.argv);
--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,53 @@
-import * as https from 'https';
-import * as querystring from 'querystring';
-import md5 = require('md5');
-import {appId, appSecret} from './private';
+import * as https from "https";
+import * as querystring from "querystring";
+import md5 = require("md5");
+import { appId, appSecret } from "./private";
 
 type ErrorMap = {
-	[key: string]: string
-}
+	[key: string]: string;
+};
 
 const errorMap: ErrorMap = {
-	52001: '请求超时，请稍后再试',
-	52002: '系统错误，请稍后再试',
-	52003: '未授权用户',
+	52001: "请求超时，请稍后再试",
+	52002: "系统错误，请稍后再试",
+	52003: "未授权用户",
 };
 
 export const translate = (word: string) => {
 	const salt = Math.random();
 	const sign = md5(appId + word + salt + appSecret);
 	let from, to;
-	
+
 	if (/[a-zA-Z]/.test(word[0])) {
-		from = 'en';
-		to = 'zh';
+		from = "en";
+		to = "zh";
 	} else {
-		from = 'zh';
-		to = 'en';
+		from = "zh";
+		to = "en";
 	}
-	
+
 	const query: string = querystring.stringify({
 		q: word,
 		appid: appId,
-		from, to, salt, sign
+		from,
+		to,
+		salt,
+		sign,
 	});
-	
+
 	const options = {
-		hostname: 'api.fanyi.baidu.com',
+		hostname: "api.fanyi.baidu.com",
 		port: 443,
 		path: `/api/trans/vip/translate?${query}`,
-		method: 'GET'
+		method: "GET",
 	};
-	
+
 	const request = https.request(options, (response) => {
 		let chunks: Buffer[] = [];
-		response.on('data', (chunk: Buffer) => {
+		response.on("data", (chunk: Buffer) => {
 			chunks.push(chunk);
 		});
-		response.on('end', () => {
+		response.on("end", () => {
 			const string = Buffer.concat(chunks).toString();
 			type BaiduResult = {
 				error_code?: string;
@@ -52,10 +55,10 @@ export const translate = (word: string) => {
 				from: string;
 				to: string;
 				trans_result: {
-					src: string
+					src: string;
 					dst: string;
-				}[]
-			}
+				}[];
+			};
 			const object: BaiduResult = JSON.parse(string);
 			if (object.error_code) {
 				console.error(errorMap[object.error_code] || object.error_msg);
@@ -67,7 +70,7 @@ export const translate = (word: string) => {
 			}
 		});
 	});
-	request.on('error', (error) => {
+	request.on("error", (error) => {
 		console.error(error);
 	});
 	request.end();
